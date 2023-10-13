e21938b2e2b626cdb2c44c5b47ac8b9b324fe043
rajeev262-cyber <142470416+rajeev262-cyber@users.noreply.github.com>
2023-10-13 12:40:22 +0530

    Add files via upload

---
+++ question10.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std ;
+int main ()
+{
+    int n ;
+    cout <<"enter the height ";
+    cin >> n;
+    for (int i=1 ; i<= n ; i++)
+    {
+        int a = i ;
+        for (int j=1 ; j<=i ; j++)
+        {
+            cout << a << " ";
+            a--;
+        }
+        cout << endl ;
+    }
+    return 0 ;
+}
---
+++ question4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std ;
+int main ()
+{
+    int n ;
+    cout <<"enter the height of triangle " ;
+    cin >> n ;
+    for (int i=1 ; i<=n ; i++)
+    {
+        for (int j=1 ; j<= i ; j++)
+        {
+            if (i%2== 0) cout << (char)(j+64) << " " ;
+            else cout << j << " ";
+        }
+        cout << endl ;
+    }
+    return 0 ;
+}
